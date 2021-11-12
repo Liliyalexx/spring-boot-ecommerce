dc74b4f8466d638ed64d2b3ec774cf6086fc649c
Liliyalexx <liliya.fedyurina@gmail.com>
2021-11-11 21:08:22 -0800

    Save the Order to Database-REST Controller

--- src/main/java/com/med/ecommerce/service/CheckoutServiceImpl.java
+++ src/main/java/com/med/ecommerce/service/CheckoutServiceImpl.java
@@ -3,7 +3,14 @@ package com.med.ecommerce.service;
 import com.med.ecommerce.dao.CustomerRepository;
 import com.med.ecommerce.dto.Purchase;
 import com.med.ecommerce.dto.PurchaseResponse;
+import com.med.ecommerce.entity.Customer;
+import com.med.ecommerce.entity.Order;
+import com.med.ecommerce.entity.OrderItem;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.Set;
+import java.util.UUID;
 
 @Service
 public class CheckoutServiceImpl implements CheckoutService {
@@ -17,7 +24,43 @@ public class CheckoutServiceImpl implements CheckoutService {
 
 
     @Override
+    @Transactional
     public PurchaseResponse placeOrder(Purchase purchase) {
-        return null;
+        // retrieve the order info from dto
+    Order order = purchase.getOrder();
+
+        // generate tracking number
+        String orderTrackingNumber = generateOrderTrackingNumber();
+        order.setOrderTrackingNumber(orderTrackingNumber);
+
+        // populate order with orderItems
+
+        Set<OrderItem> orderItems = purchase.getOrderItems();
+        orderItems.forEach(item -> order.add(item));
+
+        // populate order with billingAddress and shippingAddress
+
+       order.setBillingAddress(purchase.getBillingAddress());
+
+       order.setShippingAddress(purchase.getShippingAddress());
+
+        // populate customer with order
+
+        Customer customer = purchase.getCustomer();
+
+        // save to the database
+        customerRepository.save(customer);
+
+        // return a response
+
+        return new PurchaseResponse(orderTrackingNumber);
+    }
+
+    private String generateOrderTrackingNumber() {
+
+        // generate a random UUID number(UUID version-4)
+
+        return UUID.randomUUID().toString();
+
     }
 }
